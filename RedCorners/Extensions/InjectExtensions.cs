74307582e19977c21eeaed4cdea632e6eed97af7
Saeed Afshari (BLAZE) <s_a@live.com>
2019-06-17 22:07:20 +0200

    4.1.4 InjectExtensions dictionary

--- RedCorners/Extensions/InjectExtensions.cs
+++ RedCorners/Extensions/InjectExtensions.cs
@@ -35,7 +35,7 @@ namespace RedCorners
             }
         }
 
-        public static void Inject<V>(this Dictionary<string, V> configuration, object destination) 
+        public static void Inject(this IDictionary<string, object> configuration, object destination) 
         {
             var destProps = destination.GetType().GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
             foreach (var item in configuration.Keys)
