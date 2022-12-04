7916a12c062d8c5cbc9afe464dbd7e9789a512e6
s_a@live.com (R) <s_a@live.com>
2022-12-04 07:28:24 +0200

    v16 add IRecord

---
+++ RedCorners/Models/IRecord.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RedCorners.Models
+{
+    public interface IRecord
+    {
+        string Id { get; set; }
+    }
+}
